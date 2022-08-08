92798972b40eb7b12a2a55820384b4309da9ff55
niinuma toshitaka <niinuma.toshitaka@gmail.com>
2022-08-08 12:56:08 +0900

    Jenkins環境構築手順書をビルドする

--- build_asciidoc.sh
+++ build_asciidoc.sh
@@ -3,22 +3,27 @@
 # AsciiDocファイルをビルドする。
 
 # ビルド対象のAsciiDocファイル
-target_file=docker_setup.adoc
+target_files[0]=docker_setup.adoc
+target_files[1]=jenkins_setup.adoc
 # ビルド後のファイルを格納するディレクトリ
-destination=build/
+readonly destination=build/
 # 変換後のAsciiDocファイルに情報を埋め込む対象のコミット
-target_commit=HEAD
+readonly target_commit=HEAD
 # コミットID
 commit_hash=$(git rev-parse --short ${target_commit})
 # コミット日時
 commit_date=$(git log -1 --format='%cI' ${target_commit})
 
-# HTML形式に変換する。
-asciidoctor -r asciidoctor-diagram --trace --verbose "${target_file}" \
+# ビルド対象の各ファイルに対して
+for i in "${!target_files[@]}"
+do
+    # HTML形式に変換する。
+    asciidoctor -r asciidoctor-diagram --trace --verbose "${target_files[$i]}" \
     -a commit-hash="${commit_hash}" -a commit-date="${commit_date}"
-# PDF形式に変換する。
-asciidoctor-pdf -r asciidoctor-diagram -r ./config.rb --trace --verbose "${target_file}" \
+    # PDF形式に変換する。
+    asciidoctor-pdf -r asciidoctor-diagram -r ./config.rb --trace --verbose "${target_files[$i]}" \
     -a commit-hash="${commit_hash}" -a commit-date="${commit_date}"
+done
 
 # ビルドした成果物を移動
 mv ./*.html ./*.pdf "${destination}"
