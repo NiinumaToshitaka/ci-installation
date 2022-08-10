95baf389c90ce5fa1e9977ea415fa537390f1f58
niinuma toshitaka <niinuma.toshitaka@gmail.com>
2022-08-10 16:45:03 +0900

    各章を1つのファイルにまとめて変換する
    Docker, Jenkins, Giteaのそれぞれの環境構築手順書を作成したが、全体としてどのようなシステム構成になっているのかわからない状態だったので、システム構成図を追加した。
    システム構成図を追加するのであれば、各手順書を1つのファイルにまとめたほうがスッキリするのでまとめる。

--- build_asciidoc.sh
+++ build_asciidoc.sh
@@ -3,9 +3,9 @@
 # AsciiDocファイルをビルドする。
 
 # ビルド対象のAsciiDocファイル
-target_files[0]=docker_setup.adoc
-target_files[1]=jenkins_setup.adoc
-target_files[2]=gitea_setup.adoc
+target_file=top.adoc
+# 出力ファイルの拡張子を除いたファイル名
+output_basename="Docker+Jenkins+GiteaによるCI環境構築"
 # ビルド後のファイルを格納するディレクトリ
 readonly destination=build/
 # 変換後のAsciiDocファイルに情報を埋め込む対象のコミット
@@ -16,15 +16,12 @@ commit_hash=$(git rev-parse --short ${target_commit})
 commit_date=$(git log -1 --format='%cI' ${target_commit})
 
 # ビルド対象の各ファイルに対して
-for i in "${!target_files[@]}"
-do
-    # HTML形式に変換する。
-    asciidoctor -r asciidoctor-diagram --trace --verbose "${target_files[$i]}" \
-    -a commit-hash="${commit_hash}" -a commit-date="${commit_date}"
-    # PDF形式に変換する。
-    asciidoctor-pdf -r asciidoctor-diagram -r ./config.rb --trace --verbose "${target_files[$i]}" \
-    -a commit-hash="${commit_hash}" -a commit-date="${commit_date}"
-done
+# HTML形式に変換する。
+asciidoctor -r asciidoctor-diagram --trace --verbose "${target_file}" \
+-a commit-hash="${commit_hash}" -a commit-date="${commit_date}" -o ${output_basename}.html
+# PDF形式に変換する。
+asciidoctor-pdf -r asciidoctor-diagram -r ./config.rb --trace --verbose "${target_file}" \
+-a commit-hash="${commit_hash}" -a commit-date="${commit_date}" -o ${output_basename}.pdf
 
 # ビルドした成果物を移動
 mv ./*.html ./*.pdf "${destination}"
